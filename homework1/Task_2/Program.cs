328cac7f4df726f187ff9927869097418d0d2edf
Some_body <some_body@gmail.com>
2022-12-22 17:34:12 +0300

    Solve Task_2.

--- homework1/Task_2/Program.cs
+++ homework1/Task_2/Program.cs
@@ -1,4 +1,15 @@
 ﻿/* Практическое задание 1.
    Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. */
 
-Console.WriteLine("Hello, World!");
+Console.Write("Input the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input the third number: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+double max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine($"max = {max}");
