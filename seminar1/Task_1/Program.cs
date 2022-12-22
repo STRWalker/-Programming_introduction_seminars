cea33bcec895306a5ad49756c21477ef6ca38461
Some_body <some_body@gmail.com>
2022-12-22 13:27:09 +0300

    Initial commit Task_1

---
+++ seminar1/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+int a = 5;
+double b = 23.6;
+bool c = true;
+string d = "Qwerty";
+char e = 'd';
+
+Console.WriteLine($"My number are {a} and {b} and it's good!");
+*/
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
