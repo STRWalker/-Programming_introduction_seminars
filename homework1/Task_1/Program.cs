900a6eafd51c67d293cf08764406c372ddd1d913
Some_body <some_body@gmail.com>
2022-12-22 17:54:47 +0300

    Resolve Task_1.

--- homework1/Task_1/Program.cs
+++ homework1/Task_1/Program.cs
@@ -6,14 +6,5 @@ double num1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input the second number: ");
 double num2 = Convert.ToDouble(Console.ReadLine());
 
-double max = 0;
-double min = 0;
-if (num1 > num2) {
-   max = num1;
-   min = num2;
-} else {
-   max = num2;
-   min = num1;
-}
-
-Console.WriteLine($"max = {max}\nmin = {min}");
+if (num1 > num2) Console.WriteLine($"max = {num1}\nmin = {num2}");
+else Console.WriteLine($"max = {num2}\nmin = {num1}");
