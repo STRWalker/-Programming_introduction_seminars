dd71360467f1921348f96ca330f08ab08214a4bb
Some_body <some_body@gmail.com>
2022-12-22 17:29:15 +0300

    Solve Task_1.

--- homework1/Task_1/Program.cs
+++ homework1/Task_1/Program.cs
@@ -1,4 +1,19 @@
 ﻿/* Практическое задание 1.
    Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. */
 
-Console.WriteLine("Hello, World!");
+Console.Write("Input the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double max = 0;
+double min = 0;
+if (num1 > num2) {
+   max = num1;
+   min = num2;
+} else {
+   max = num2;
+   min = num1;
+}
+
+Console.WriteLine($"max = {max}\nmin = {min}");
