737c42bd40455c781dc77fdf6e2e0b98a2dc664d
Some_body <some_body@gmail.com>
2022-12-22 13:43:06 +0300

    Solve Task_1

--- seminar1/Task_1/Program.cs
+++ seminar1/Task_1/Program.cs
@@ -1,12 +1,11 @@
-﻿/*
-int a = 5;
-double b = 23.6;
-bool c = true;
-string d = "Qwerty";
-char e = 'd';
+﻿// Семинар 1.
+// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
-Console.WriteLine($"My number are {a} and {b} and it's good!");
-*/
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int quad2 = num2*num2;
+if (num1 == quad2) Console.WriteLine("Yes!");
+else Console.WriteLine("No!");
