d2c286886427f0661264655ee52345ba89c5510f
Some_body <some_body@gmail.com>
2022-12-22 14:07:00 +0300

    Solve Task_2

--- seminar1/Task_2/Program.cs
+++ seminar1/Task_2/Program.cs
@@ -1,4 +1,13 @@
-﻿// Семинар 1.
-// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+﻿/* Семинар 1.
+   Задача 2. Напишите программу, которая на вход принимает одно число (N),
+   а на выходе показывает все целые числа в промежутке от -N до N. */
 
-Console.WriteLine("Hello, World!");
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = -number;
+
+int current = -number;
+while (current <= number) {
+    Console.Write(current + " ");
+    current++;
+}
