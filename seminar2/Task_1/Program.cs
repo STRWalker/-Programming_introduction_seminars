0bef382651bc89057cef63884f900f247022c07a
Some_body <some_body@gmail.com>
2022-12-24 11:36:13 +0300

    Solve Task_1

--- seminar2/Task_1/Program.cs
+++ seminar2/Task_1/Program.cs
@@ -2,4 +2,19 @@
    Задача 1: Напишите программу, которая выводит случайное
    трёхзначное число и удаляет вторую цифру этого числа. */
 
-Console.WriteLine("Hello, World!");
+int numberGet() {
+    return new Random().Next(100, 1000);
+}
+
+int numberCut(int number) {
+    int sot = number / 100;
+    int ed = number % 10;
+
+    int result = sot*10 + ed;
+    return result;
+}
+
+int number = numberGet();
+Console.WriteLine($"number = {number}");
+int result = numberCut(number);
+Console.WriteLine($"result = {result}");
