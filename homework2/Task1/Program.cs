6f67a67442d5a6fa7e561afb230a41bc5f847236
Some_body <some_body@gmail.com>
2022-12-24 17:45:41 +0300

    Solve Task1

--- homework2/Task1/Program.cs
+++ homework2/Task1/Program.cs
@@ -2,4 +2,21 @@
    Задача 10: Напишите программу, которая принимает на вход трёхзначное число
    и на выходе показывает вторую цифру этого числа. */
 
-Console.WriteLine("Hello, World!");
+int numberGet() {
+   Console.Write("Input a three-digit number: ");
+   int number = Convert.ToInt32(Console.ReadLine());
+
+   if (number < 0) number = -number;
+   if (number/100 == 0 || number/1000 != 0) return numberGet();
+
+   //Console.WriteLine($"(debug) number = {number}");
+   return number;
+}
+
+int secondDigit(int number) {
+   return number/10 %10;
+}
+
+int number = numberGet();
+int second_digit = secondDigit(number);
+Console.WriteLine($"Second digit = {second_digit}");
