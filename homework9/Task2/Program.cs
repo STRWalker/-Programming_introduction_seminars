a619c267cce8e5f3a3f3e399387a95d7172f7d16
Some_body <some_body@gmail.com>
2023-02-11 07:28:56 +0300

    Solve homework9/Task2

--- homework9/Task2/Program.cs
+++ homework9/Task2/Program.cs
@@ -1,4 +1,23 @@
 ﻿/*  Практическое задание 9.
     Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. */
 
-Console.WriteLine("Hello, World!");
+int[] inputNumbersGet() {
+    int[] input_numbers = new int[2];
+    Console.Write("Input the first number: ");
+    input_numbers[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the second number: ");
+    input_numbers[1] = Convert.ToInt32(Console.ReadLine());
+
+    return input_numbers;
+}
+
+int calculateSumFromTo(int first_num, int second_num) {
+    if (first_num < second_num) return first_num + calculateSumFromTo(first_num + 1, second_num);
+    else if (first_num > second_num) return first_num + calculateSumFromTo(first_num - 1, second_num);
+    else return first_num;
+}
+
+int[] input_numbers = inputNumbersGet();
+int answer = calculateSumFromTo(input_numbers[0], input_numbers[1]);
+
+Console.WriteLine($"The sum of numbers from {input_numbers[0]} to {input_numbers[1]} is {answer}");
